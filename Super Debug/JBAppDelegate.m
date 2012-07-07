f42b586b37ad263d9c0fcd1e22f5009b4be53724
Jason Brennan <i.jasonbrennan@gmail.com>
2012-07-07 16:14:41 -0400

    Initial Commit

---
+++ Super Debug/JBAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  JBAppDelegate.m
+//  Super Debug
+//
+//  Created by Jason Brennan on 12-07-07.
+//  Copyright (c) 2012 Jason Brennan. All rights reserved.
+//
+
+#import "JBAppDelegate.h"
+
+@implementation JBAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
