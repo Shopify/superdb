174148aa2e66d8d8bc61c95046943a98ffde7fc8
Jason Brennan <i.jasonbrennan@gmail.com>
2012-07-07 16:45:42 -0400

    Initial Commit

---
+++ SuperDBCore/SuperDBCore.h
@@ -0,0 +1,13 @@
+//
+//  SuperDBCore.h
+//  SuperDBCore
+//
+//  Created by Jason Brennan on 12-07-07.
+//  Copyright (c) 2012 Jason Brennan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SuperDBCore : NSObject
+
+@end
---
+++ SuperDBCore/SuperDBCore.m
@@ -0,0 +1,13 @@
+//
+//  SuperDBCore.m
+//  SuperDBCore
+//
+//  Created by Jason Brennan on 12-07-07.
+//  Copyright (c) 2012 Jason Brennan. All rights reserved.
+//
+
+#import "SuperDBCore.h"
+
+@implementation SuperDBCore
+
+@end
