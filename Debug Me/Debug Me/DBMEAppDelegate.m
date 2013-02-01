c8176e41168c08869b8699e623d0dd2d0aac9817
Mathieu Godart <mathieu.godart@gmail.com>
2013-02-01 12:16:19 +0100

    Added a simple dumb method to generate an image from a UIView. This can be used to test the ".image" command and to get a little "wow" effect.

--- Debug Me/Debug Me/DBMEAppDelegate.m
+++ Debug Me/Debug Me/DBMEAppDelegate.m
@@ -10,6 +10,30 @@
 
 #import "DBMEViewController.h"
 #import <SuperDBCore/SuperDBCore.h>
+#import <QuartzCore/QuartzCore.h>
+
+
+
+@implementation UIView (Screenshot)
+
+// Generate an image of the receiver appearance.
+// TODO: Improve the target resolution of the generated photo. See that page, to do so:
+// http://stackoverflow.com/questions/2500915/how-to-create-an-image-from-a-uiview-uiscrollview
+- (UIImage *)generateImage
+{
+    UIGraphicsBeginImageContext(self.frame.size);
+    CGContextRef generatingContext = UIGraphicsGetCurrentContext();
+    
+    [self.layer renderInContext:generatingContext];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+@end
+
+
 
 @implementation DBMEAppDelegate {
 	SuperInterpreterService *_interpreterService;
