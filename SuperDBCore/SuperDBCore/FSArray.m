1dcc45d3fe954229d4a1cfb454fa22cde6f90aa7
Joe Booth <joe@joebooth.com>
2014-02-27 17:42:26 -0800

    Bug Fix: printString message was being sent to object that didn't respond to it - added respondsToSelector:.. and fullback to debugDescription

--- SuperDBCore/SuperDBCore/FSArray.m
+++ SuperDBCore/SuperDBCore/FSArray.m
@@ -336,9 +336,13 @@ typedef struct fs_objc_object {
 {
   // fallback to printString (we may do something better in the future).
   // Note: super's implementation is not used because it won't work if the array contains nil.
-  return [self printString];
+    if ([self respondsToSelector:@selector(printString)]) {
+        return [self printString];
+    } else {
+        return [self debugDescription];
+    }
 }
-  
+
 - (void *)dataPtr
 {
   if (type != FS_ID) [self becomeArrayOfId];
